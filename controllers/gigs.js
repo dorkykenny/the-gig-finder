c70f9a5e0473c42d4af9a40e22670f60187ce1ad
dorkykenny <kennethcarlsamoza8@gmail.com>
2024-09-06 10:32:31 +1000

    gigs index page added

--- controllers/gigs.js
+++ controllers/gigs.js
@@ -4,6 +4,22 @@ const router = express.Router()
 const User = require('../models/User.js')
 const Gig = require('../models/Gig.js')
 
+// C1:
+
+// C2:
+
+// R1:
+router.get('/', async (req, res) => {
+    res.render('gigs/index')
+})
+
+// R2:
+
+// U1:
+
+// U2:
+
+// D:
 
 
 module.exports = router
